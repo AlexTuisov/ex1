5c3be97b4021cd63b2d4a54b45f8e70128437c4a
Alex Tuisov <alexandt@campus.technion.ac.il>
2016-11-29 19:53:42 +0200

    Added ngrams instead of uni, bi, tri grams

--- Preprocessing.py
+++ Preprocessing.py
@@ -47,35 +47,39 @@ def prettifying_the_tagged_sentence(sentence):
         except:
             if tagged_word == "*":
                 tag = "start"
+                pure_word = "*"
             else:
                 tag = "finish"
+                pure_word = "@@@"
         sentence_as_tags.append(tag)
         sentence_without_tags.append(pure_word)
     return tuple(sentence_without_tags), tuple(sentence_as_tags)
 
-def get_unigrams():
-    unigram_features_as_dictionary = {}
+def get_path_to_training_set():
     path = os.path.dirname(__file__)
     absolute_path = os.path.join(path, "data/train.wtag")
-    with open(absolute_path) as raw_train_set:
+    return absolute_path
+"""
+def get_unigrams():
+    unigram_features_as_dictionary = {}
+    with open(get_path_to_training_set()) as raw_train_set:
         for num_of_sentence, sentence in enumerate(raw_train_set):
             train_set_as_strings = sentence.split()
+            unigram_features_as_dictionary[num_of_sentence] = {}
             for tagged_word in train_set_as_strings:
                 try:
                     word, tag = tagged_word.split('_')
                 except:
                     continue
-                if word not in unigram_features_as_dictionary.keys():
-                    unigram_features_as_dictionary[word] = [(tag, num_of_sentence),]
+                if word not in unigram_features_as_dictionary[num_of_sentence].keys():
+                    unigram_features_as_dictionary[num_of_sentence][word] = [tag, ]
                 else:
-                    unigram_features_as_dictionary[word].append((tag, num_of_sentence),)
+                    unigram_features_as_dictionary[num_of_sentence][word].append(tag)
     return unigram_features_as_dictionary
 
 def get_bigrams():
     bigram_features_as_dictionary = {}
-    path = os.path.dirname(__file__)
-    absolute_path = os.path.join(path, "data/train.wtag")
-    with open(absolute_path) as raw_train_set:
+    with open(get_path_to_training_set()) as raw_train_set:
         for num_of_sentence, sentence in enumerate(raw_train_set):
             words, tags = prettifying_the_tagged_sentence(sentence)
             for index, word in enumerate(words):
@@ -88,4 +92,37 @@ def get_bigrams():
     return bigram_features_as_dictionary
 
 def get_trigrams():
-    trigram_features_as_
+    trigram_features_as_dictionary = {}
+    with open(get_path_to_training_set()) as raw_train_set:
+        for num_of_sentence, sentence in enumerate(raw_train_set):
+            words, tags = prettifying_the_tagged_sentence(sentence)
+            for index, word in enumerate(words):
+                if (index < 2) or (index > len(words) - 2):
+                    continue
+                if word not in trigram_features_as_dictionary.keys():
+                    trigram_features_as_dictionary[word] = [(tags[index], tags[index-1], tags[index-2], num_of_sentence),]
+                else:
+                    trigram_features_as_dictionary[word].append((tags[index], tags[index-1], tags[index-2], num_of_sentence),)
+    return trigram_features_as_dictionary
+"""
+def get_ngrams(n):
+    ngrams_as_dictionary = {}
+    with open(get_path_to_training_set()) as raw_train_set:
+        for num_of_sentence, sentence in enumerate(raw_train_set):
+            words, tags = prettifying_the_tagged_sentence(sentence)
+            ngrams_as_dictionary[num_of_sentence] = {}
+            for index, word in enumerate(words):
+                if (index < 2) or (index > len(words) - 2):
+                    continue
+                feature = []
+                for i in range(0, n):
+                    if (index - i) >= 0:
+                        feature.append(tags[index - i])
+                    else:
+                        feature.append("start")
+                feature = tuple(feature)
+                if word not in ngrams_as_dictionary[num_of_sentence].keys():
+                    ngrams_as_dictionary[num_of_sentence][word] = [feature, ]
+                else:
+                    ngrams_as_dictionary[num_of_sentence][word].append(feature)
+    return ngrams_as_dictionary
--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import Preprocessing
-
+import random
 if __name__ == '__main__':
-    Preprocessing.preprocessing()
+    unigrams = Preprocessing.get_ngrams(1)
+    bigrams = Preprocessing.get_ngrams(2)
+    trigrams = Preprocessing.get_ngrams(3)
+    print(unigrams.popitem())
+    print("\n")
+    print(bigrams.popitem())
+    print("\n")
+    print(trigrams.popitem())
