2dca3cfa689a0deda9e52b6ce8a2e1a7c4f9d631
gregory goren <grggoren@gmail.com>
2016-11-29 21:07:25 +0200

    created feature vector

--- GradientAscent.py
+++ GradientAscent.py
@@ -1,13 +1,21 @@
-import numpy as np
 import Preprocessing as pr
 from scipy.sparse import bsr_matrix
-def createSparseVectorOfFeatures(bigrams,unigrams,trigrams,sentenceNumber,numberOfParams):
-    featureVec = bsr_matrix((1, numberOfParams))
-    sentenceTags = unigrams[sentenceNumber]
+def createSparseVectorOfFeatures(bigrams,unigrams,trigrams,sentenceNumber,numberOfParams,paramIndex):
+    special_delimiter = "###"#TODO: will be decided outside
+    feature_vec = bsr_matrix((1, numberOfParams)).toarray()
+    sentence_tags = unigrams[sentenceNumber]
+    for word in sentence_tags:
+        vector_index = paramIndex[word+special_delimiter+sentence_tags[word][0]]
+        feature_vec[vector_index] = 1
+    sentence_tags = bigrams[sentenceNumber]
+    for word in sentence_tags:
+        vector_index = paramIndex[word + special_delimiter + sentence_tags[word][0]+special_delimiter + sentence_tags[word][1]]
+        feature_vec[vector_index] = 1
+    sentence_tags = trigrams[sentenceNumber]
+    for word in sentence_tags:
+        vector_index = paramIndex[
+            word + special_delimiter + sentence_tags[word][0] + special_delimiter + sentence_tags[word][1]+special_delimiter + sentence_tags[word][2]]
+        feature_vec[vector_index] = 1
+    return feature_vec
 
 
-    return ""
-
-
-def getNumberOfDimentions():
-    return 27000 #TODO :this will be infered later
