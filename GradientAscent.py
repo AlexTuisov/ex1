229461b8191319cf0ed13ee432dc5929326e5b53
gregory goren <grggoren@gmail.com>
2016-11-30 17:30:24 +0200

    added regularized log likelihood

--- GradientAscent.py
+++ GradientAscent.py
@@ -21,23 +21,37 @@ class gradient_ascent:
         return np.dot(vector_a,vector_b.transpose())
 
 
-    def log_of_denominator(self,previous_tag,last_tag,sentence,index,param_index,vector_v):
+    def log_of_denominator(self,previous_tag,third_tag,sentence,index,param_index):
         total_sum = 0
         for tag in self.tags:
-            feature_vec = self.feature_maker.create_sparse_vector_of_features(tag,previous_tag,last_tag,sentence,index,param_index,self.number_of_dimensions)
-            inner_product = self.vector_multiplication(vector_v,feature_vec)
+            feature_vec = self.feature_maker.create_sparse_vector_of_features(tag,previous_tag,third_tag,sentence,index,param_index,self.number_of_dimensions)
+            inner_product = self.vector_multiplication(self.vector_v,feature_vec)
             total_sum+=np.exp(inner_product)
         return np.log(total_sum)
 
-    def log_of_numerator(self,tag, previous_tag, last_tag, sentence, index,
-                                                                          param_index,vector_v):
-        feature_vec = self.feature_maker.create_sparse_vector_of_features(tag, previous_tag, last_tag, sentence, index,
+    def log_of_numerator(self,tag, previous_tag, third_tag, sentence, index,
+                                                                          param_index):
+        feature_vec = self.feature_maker.create_sparse_vector_of_features(tag, previous_tag, third_tag, sentence, index,
                                                                           param_index, self.number_of_dimensions)
-        inner_product = self.vector_multiplication(vector_v, feature_vec)
+        inner_product = self.vector_multiplication(self.vector_v, feature_vec)
         return inner_product
 
-    def regularized_log_liklihood(self,sentences):
-        for index in sentences:
-            sentence = sentences[index]
+
+    def get_regularized_log_likelihood_function(self):
+        def regularized_log_likelihood(self,sentences,trigrams,param_index):
+            total_sum = 0
+            for index in sentences:
+                sentence = sentences[index]
+                for i,word in enumerate(sentence):
+                    previous_tag = trigrams[index][word][0][1]
+                    third_tag = trigrams[index][word][0][2]
+                    current_tag = trigrams[index][word][0][0]
+                    log_of_numerator = self.log_of_numerator(current_tag,previous_tag,third_tag,sentence,i,param_index)
+                    log_of_denomenator = self.log_of_denominator(previous_tag,third_tag,sentence,index,param_index)
+                    total_sum += (log_of_numerator - log_of_denomenator)
+
+            regularization = self.lambda_value*0.5*self.vector_multiplication(self.vector_v,self.vector_v)
+            return (total_sum-regularization)
+        return regularized_log_likelihood
 
 
