97eb5b1d374f0cec6d62e9678ff92fda2a2de86c
Alex Tuisov <alexandt@campus.technion.ac.il>
2016-12-01 13:43:28 +0200

    Added sketch for searcher class

---
+++ Search.py
@@ -0,0 +1,12 @@
+#File that contains search algorithms once the model has been learned
+
+class searcher:
+    def __init__(self):
+        #skeleton for constructor
+        return None
+
+    def viterbi_run(self):
+        #skeleton for Viterbi algorithm
+        return None
+
+    
