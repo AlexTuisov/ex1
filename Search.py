94bdfa6b24a5989eaf5c0353073814bd52738af8
Alex Tuisov <alexandt@campus.technion.ac.il>
2016-12-01 14:13:15 +0200

    minor

--- Search.py
+++ Search.py
@@ -1,12 +1,14 @@
 #File that contains search algorithms once the model has been learned
 
 class searcher:
-    def __init__(self):
+    def __init__(self, set_of_tags):
         #skeleton for constructor
+        self.tags = set_of_tags
         return None
 
-    def viterbi_run(self):
+    def viterbi_run(self, sentence_as_list_of_pure_words):
         #skeleton for Viterbi algorithm
+        number_of_iterations = len(sentence_as_list_of_pure_words)
+        
         return None
 
-    
