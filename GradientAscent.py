gradient ascent bug fix

GradientAscent.py
@@ -1,10 +1,14 @@
 import Preprocessing as pr
 import numpy as np
-import scipy as sp
+from scipy.optimize import fmin_l_bfgs_b
 import featureMaker
 from scipy.sparse import bsr_matrix
 
-class gradient_ascent:
+
+def vector_multiplication(vector_a, vector_b):
+    return np.dot(vector_a, vector_b.transpose())
+
+class gradient_ascent: #TODO: handle non seen tags!!!!!
     def __init__(self,number_of_dimensions,lambda_value,tags):
         self.number_of_dimensions=number_of_dimensions
         self.lambda_value = lambda_value
@@ -18,15 +22,11 @@ class gradient_ascent:
         return vector_v
 
 
-    def vector_multiplication(self,vector_a,vector_b):
-        return np.dot(vector_a,vector_b.transpose())
-
-
     def log_of_denominator(self,previous_tag,third_tag,sentence,index,param_index):
         total_sum = 0
         for tag in self.tags:
             feature_vec = self.feature_maker.create_sparse_vector_of_features(tag,previous_tag,third_tag,sentence,index,param_index,self.number_of_dimensions)
-            inner_product = self.vector_multiplication(self.vector_v,feature_vec)
+            inner_product = vector_multiplication(self.vector_v,feature_vec)
             total_sum+=np.exp(inner_product)
         return np.log(total_sum)
 
@@ -34,47 +34,48 @@ class gradient_ascent:
                                                                           param_index):
         feature_vec = self.feature_maker.create_sparse_vector_of_features(tag, previous_tag, third_tag, sentence, index,
                                                                           param_index, self.number_of_dimensions)
-        inner_product = self.vector_multiplication(self.vector_v, feature_vec)
+        inner_product = vector_multiplication(self.vector_v, feature_vec)
         return inner_product
 
 
-    def get_regularized_log_likelihood_function(self):
-        def regularized_log_likelihood(self,sentences,trigrams,param_index):
-            total_sum = 0
-            for index in sentences:
-                sentence = sentences[index]
-                for i,word in enumerate(sentence):
-                    previous_tag = trigrams[index][word][0][1]
-                    third_tag = trigrams[index][word][0][2]
-                    current_tag = trigrams[index][word][0][0]
-                    log_of_numerator = self.log_of_numerator(current_tag,previous_tag,third_tag,sentence,i,param_index)
-                    log_of_denomenator = self.log_of_denominator(previous_tag,third_tag,sentence,index,param_index)
-                    total_sum += (log_of_numerator - log_of_denomenator)
+    def regularized_log_likelihood(self,sentences,trigrams,param_index):
+        total_sum = 0
+        for index in sentences:
+            sentence = sentences[index]
+            for i,word in enumerate(sentence):
+                previous_tag = trigrams[index][word][0][1]
+                third_tag = trigrams[index][word][0][2]
+                current_tag = trigrams[index][word][0][0]
+                log_of_numerator = self.log_of_numerator(current_tag,previous_tag,third_tag,sentence,i,param_index)
+                log_of_denomenator = self.log_of_denominator(previous_tag,third_tag,sentence,index,param_index)
+                total_sum += (log_of_numerator - log_of_denomenator)
 
-            regularization = self.lambda_value*0.5*self.vector_multiplication(self.vector_v,self.vector_v)
-            return (-total_sum+regularization)
-        return regularized_log_likelihood
+        regularization = self.lambda_value*0.5*self.vector_multiplication(self.vector_v,self.vector_v)
+        return (-total_sum+regularization)
 
-    def gradient_of_log_likelihood_function_by_vj(self):
-        def gradient_calculator(reverese_param_index,param_index,sentences):
-            total_sum = 0
-            if self.feature_index > len(param_index):
-                self.feature_index = 0
-            feature_counter = param_index[reverese_param_index[self.feature_index]][1]
-            feature = reverese_param_index[self.feature_index]
-            feature_components = feature.split(self.feature_maker.special_delimiter)
-            for tag in self.tags:
-                if (len(feature_components)>=3):
-                    new_feature = feature_components[0]+self.feature_maker.special_delimiter+tag+feature_components[2:]
-                else:
-                    new_feature = feature_components[0]+self.feature_maker.special_delimiter+tag
-                new_feature_counter = param_index[new_feature][1]
-                new_feature_index = param_index[new_feature][0]
-                total_sum += new_feature_counter*self.probability_calculation(new_feature_index,new_feature,param_index)
-            return self.lambda_value*self.vector_v[self.feature_index]- total_sum - feature_counter
+    def gradient_of_log_likelihood_function_by_vj(self,reverese_param_index,param_index):
+
+        total_sum = 0
+        if self.feature_index > len(param_index):
+            self.feature_index = 0
+        feature_counter = param_index[reverese_param_index[self.feature_index]][1]
+        feature = reverese_param_index[self.feature_index]
+        feature_components = feature.split(self.feature_maker.special_delimiter)
+        for tag in self.tags:
+            if (len(feature_components)>=3):
+                new_feature = feature_components[0]+self.feature_maker.special_delimiter+tag+feature_components[2:]
+            else:
+                new_feature = feature_components[0]+self.feature_maker.special_delimiter+tag
+            new_feature_counter = param_index[new_feature][1]
+            new_feature_index = param_index[new_feature][0]
+            total_sum += new_feature_counter*self.probability_calculation(new_feature_index,new_feature,param_index)
+        return self.lambda_value*self.vector_v[self.feature_index]- total_sum - feature_counter
 
     def probability_calculation(self,index_of_feature,feature,param_index):
-        numerator = np.exp(self.vector_multiplication(self.vector_v[index_of_feature]))
+        if param_index.get(index_of_feature , False):
+            numerator = np.exp(self.vector_v[index_of_feature])
+        else:
+            numerator = float(0.1/0.9)
         denominator = 0
         feature_components = feature.split(self.feature_maker.special_delimiter)
         for tag in self.tags:
@@ -83,8 +84,12 @@ class gradient_ascent:
                                                                                                    2:]
             else:
                 new_feature = feature_components[0] + self.feature_maker.special_delimiter + tag
-            new_feature_vec = feature_vec = bsr_matrix((1, self.number_of_dimensions)).toarray()
+            new_feature_vec = bsr_matrix((1, self.number_of_dimensions)).toarray()
             new_feature_index = param_index[new_feature]
             new_feature_vec[new_feature_index]=1
-            denominator += np.exp(self.vector_multiplication(self.vector_v,new_feature_vec))
-        return float(float(numerator)/denominator)
+            denominator += np.exp(vector_multiplication(self.vector_v,new_feature_vec))
+        return float(float(numerator)/denominator)*0.9
+
+
+    def gradient_ascent(self,sentences,trigram,param_index,reverese_param_index):
+        fmin_l_bfgs_b(self.regularized_log_likelihood(sentences,trigram,param_index), self.vector_v, fprime=self.gradient_of_log_likelihood_function_by_vj(reverese_param_index,param_index), factr=1000000000.0)