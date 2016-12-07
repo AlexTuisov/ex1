eadff7fc5a93f7177c0783ada310c826636b30a7
Alex Tuisov <alexandt@campus.technion.ac.il>
2016-12-07 20:49:53 +0200

    Almost working viterbi

--- Search.py
+++ Search.py
@@ -1,11 +1,15 @@
 # File that contains search algorithms once the model has been learned
+from copy import deepcopy
+
 import numpy as np
 import random
+
 
 class Searcher:
     def __init__(self, tags, gradient_descent):
         # tags should be a tuple instead of set
         self.tags = tags
+        self.tags_as_tuple = tuple(sorted(list(tags.values())))
         self.gradient_descent = gradient_descent
 
     def viterbi_full_run(self, pure_test_set, test_set_with_true_tags):
@@ -22,31 +26,31 @@ class Searcher:
         previous_pi_value_table[self.tags["start"], self.tags["start"]] = 1
 
         # first step
-        table_of_backpointers = np.zeros((length_of_sentence, len(self.tags), len(self.tags)))
+        table_of_backpointers = np.zeros((length_of_sentence+1, len(self.tags), len(self.tags)))
 
         # the iterations
         current_pi_value_table = np.empty((len(self.tags), len(self.tags)))
-        for k in range(1, length_of_sentence):
+        for k in range(0, length_of_sentence):
             current_pi_value_table = np.empty((len(self.tags), len(self.tags)))
-            for index_of_u, u in enumerate(self.tags):
-                for index_of_v, v in enumerate(self.tags):
-                    current_pi_value_table[index_of_u][index_of_v], table_of_backpointers[k, index_of_u, index_of_v] = self.calculate_pi_value_and_backpointer(
-                        k, u, v, previous_pi_value_table[index_of_u], sentence_as_list_of_pure_words[k])
-            previous_pi_value_table = np.copy(current_pi_value_table)
+            for u in self.tags_as_tuple:
+                for v in self.tags_as_tuple:
+                    current_pi_value_table[u][v], table_of_backpointers[k][u][v] = self.calculate_pi_value_and_backpointer(
+                        u, v, previous_pi_value_table[u], sentence_as_list_of_pure_words[k])
+            previous_pi_value_table = deepcopy(current_pi_value_table)
 
         # last step
-        for index_of_u, u in enumerate(self.tags):
-            for index_of_v, v in enumerate(self.tags):
-                current_pi_value_table[index_of_u][index_of_v] = (previous_pi_value_table[index_of_u][index_of_v]
-                                                                    + self.log_transition_probabilities("@@@", u, v, "finish"))
-        last_tags = np.unravel_index(current_pi_value_table.argmax(), previous_pi_value_table.shape)
+        for u in self.tags_as_tuple:
+            for v in self.tags_as_tuple:
+                current_pi_value_table[u][v] = (previous_pi_value_table[u][v]
+                                                                    + self.log_transition_probabilities(self.tags["@@@"], u, v, "finish"))
+        last_tags = np.unravel_index(current_pi_value_table.argmax(), current_pi_value_table.shape)
         sentence_as_tags = self.extract_backpointers(table_of_backpointers, last_tags, length_of_sentence)
         return sentence_as_tags
 
-    def calculate_pi_value_and_backpointer(self, k, u, v, previous_pi_value_column, word):
+    def calculate_pi_value_and_backpointer(self, u, v, previous_pi_value_column, word):
         pi_values = {}
-        for index_of_t, t in enumerate(self.tags):
-            pi_values[previous_pi_value_column[index_of_t]+self.log_transition_probabilities(v, u, t, word)] = self.tags[t]
+        for t in self.tags_as_tuple:
+            pi_values[previous_pi_value_column[t]+self.log_transition_probabilities(v, u, t, word)] = t
         best_value = max(list(pi_values.keys()))
         return best_value, pi_values[best_value]
 
@@ -54,22 +58,28 @@ class Searcher:
         last_tag = int(last_tags[1])
         tag_before_last = int(last_tags[0])
         sentence_as_tags = [last_tag, tag_before_last]
-        for index in reversed(range(0, length_of_sentence-2)):
-            new_tag = table_of_backpointers[index+2][last_tag][tag_before_last]
+        for index in reversed(range(1, length_of_sentence-1)):
+            new_tag = table_of_backpointers[index][last_tag][tag_before_last]
             sentence_as_tags.append(int(new_tag))
             last_tag = int(new_tag)
             tag_before_last = int(last_tag)
         sentence_as_tags.reverse()
         return sentence_as_tags
 
-    def log_transition_probabilities(self, self_tag, previous_tag, last_tag, word):
+    def log_transition_probabilities(self, self_tag, last_tag, previous_tag, word):
         # will talk to Greg's function and calculate log
         # stub
-        prob = 0.1
-        if word == "start" and self_tag == self.tags["start"]:
-            prob = 0.9
-        if word == "the" and (self_tag == self.tags["DT"] or last_tag == self.tags["start"]):
-            prob = 0.8
+        prob = 0.1 + random.random()*0.05
+        if word == "the" and self_tag == self.tags["DT"]:
+            prob += 0.5
+        if word == "dog"  and self_tag == self.tags["NN"] and previous_tag == self.tags["DT"]:
+            prob += 0.5
+        if word == "barks" and self_tag == self.tags["VB"] and last_tag == self.tags["DT"]:
+            prob += 0.5
+        if word == "finish" and previous_tag == self.tags["NN"] and self_tag == self.tags["@@@"]:
+            prob += 0.6
+        if word == "finish" and last_tag == self.tags["NN"] and self_tag == self.tags["@@@"]:
+            prob += 0.2
         return np.log(prob)
 
 
--- main.py
+++ main.py
@@ -3,15 +3,20 @@ import Search
 import random
 if __name__ == '__main__':
     # tags = Preprocessing.preprocessing()
-    unigrams, words = Preprocessing.get_ngrams(1)
-    bigrams, words = Preprocessing.get_ngrams(2)
-    trigrams, words = Preprocessing.get_ngrams(3)
+    #unigrams, words = Preprocessing.get_ngrams(1)
+    #bigrams, words = Preprocessing.get_ngrams(2)
+    #strigrams, words = Preprocessing.get_ngrams(3)
 
     tags_as_numbers = (0, 1, 2, 3, 4)
     tags = {"start": 0, "VB": 1, "NN": 2, "DT": 3, "@@@": 4}
     invert_tags = {number : tag for tag, number in tags.items()}
-    sentence = ("the", "dog", "barks", "barks", "dog", "the", "dog", "the", "barks")
+    sentence = ("the", "dog", "barks", "the")*5
+
+
     my_little_viterbi = Search.Searcher(tags, None)
+    print (my_little_viterbi.tags_as_tuple)
     output_as_numbers = my_little_viterbi.viterbi_run_per_sentence(sentence)
     for index, output in enumerate(output_as_numbers):
         print(sentence[index], invert_tags[output])
+
+
