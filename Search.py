17456ff64a1e740f513f1b11aaeaad45643f43f3
Alex Tuisov <alexandt@campus.technion.ac.il>
2016-12-25 17:27:25 +0200

    mostly documentation on search

--- Search.py
+++ Search.py
@@ -33,6 +33,7 @@ class Searcher:
 
         self.feature_maker = gradient_ascent.feature_maker
 
+    # function that outputs the result of the inference on the true test set
     def viterbi_output_run(self, output_set):
         with open(Preprocessing.get_path_to_output(), 'w') as output_file:
             with Pool(4) as p:
@@ -40,7 +41,7 @@ class Searcher:
                 for tagged_sentence in results_list:
                     output_file.write((" ").join(tagged_sentence))
                     output_file.write("\n")
-
+    # function that outputs the result of the inference on the validation set
     def viterbi_full_run(self, pure_test_set, test_set_with_true_tags):
         accuracy_list = []
         with Pool(4) as p:
@@ -62,7 +63,7 @@ class Searcher:
         print(random.choice(results_list))"""
         return average_accuracy
 
-
+    # inference per sentence
     def viterbi_run_per_sentence(self, sentence_as_list_of_pure_words_without_finish):
         sentence_as_list_of_pure_words = list(sentence_as_list_of_pure_words_without_finish) + ["finish", "finish"]
         length_of_sentence = len(sentence_as_list_of_pure_words)
@@ -71,12 +72,9 @@ class Searcher:
         previous_pi_value_table = np.full((len(self.tags), len(self.tags)), 0, dtype=np.float)
         previous_pi_value_table[self.tags["start"], self.tags["start"]] = 1
         table_of_backpointers = np.zeros((length_of_sentence-1, len(self.tags), len(self.tags)), dtype=np.int16)
-
-        # the iterations
         current_pi_value_table = np.zeros((len(self.tags), len(self.tags)))
 
-        #extracting the relevant tags for u and v
-
+        # pi value calculations for the whole sentence
         for k in range(0, length_of_sentence-1):
             current_pi_value_table = np.full((len(self.tags), len(self.tags)), 0, dtype=np.float)
             relevant_tags_for_u = self.extract_relevant_tags(k-1, sentence_as_list_of_pure_words)
@@ -98,6 +96,7 @@ class Searcher:
         tagged_sentence = self.combine_words_with_tags(sentence_as_list_of_pure_words_without_finish, sentence_as_tags)
         return tagged_sentence
 
+        # single pi value calculation
     def calculate_pi_value_and_backpointer(self, u, v, previous_pi_value_row, word, index, sentence):
         pi_values = {}
         relevant_tags_for_t = self.extract_relevant_tags(index - 2, sentence)
@@ -111,13 +110,14 @@ class Searcher:
             permutation_matrix = exponential_matrix_dictionary[t]
             denominator_dictionary[t] = self.gradient_ascent.sum_of_exponential_permutations(permutation_matrix,
                                                                                              self.vector_v)
-        exponential_matrix_dictionary = {}  # memmory purpose
+        exponential_matrix_dictionary = {}  # memory purpose
         for t in relevant_tags_for_t:
             pi_values[previous_pi_value_row[t] + self.log_transition_probabilities(v, u, t, word,
                                                                                    denominator_dictionary[t])] = t
         best_value = max(list(pi_values.keys()))
         return (best_value, pi_values[best_value])
 
+        #extracting backpointers for the whole sentence
     def extract_backpointers(self, table_of_backpointers, last_tags, length_of_sentence, sentence):
         last_tag = int(last_tags[0])
         tag_before_last = int(last_tags[1])
@@ -134,17 +134,20 @@ class Searcher:
         sentence_as_tags.reverse()
         return sentence_as_tags[1:-1]
 
+        # calculation of the probabilities
     def log_transition_probabilities(self, self_tag, previous_tag, last_tag, word, denominator):
         # implementation of formula from slides of tirgul 4
         numerator = np.exp(self.get_feature_vector(self_tag, previous_tag, last_tag, word).dot(self.vector_v))
         value = np.log(float(numerator)/denominator)
         return value
 
+        # feature vector for specific history
     def get_feature_vector(self, tag, previous_tag, last_tag, word):
         new_vector = self.feature_maker.create_sparse_vector_of_features(self.inverted_tags[tag],
                                                                          self.inverted_tags[previous_tag], self.inverted_tags[last_tag], word)
         return new_vector
 
+        # self-evident
     def combine_words_with_tags(self, sentence, current_tag_sequence):
         tagged_sentence = []
         for index, output in enumerate(current_tag_sequence):
@@ -152,6 +155,7 @@ class Searcher:
             tagged_sentence.append(to_append)
         return tagged_sentence
 
+        # function that helps us with statistics in the end (validation set only)
     def check_outcome(self, tagged_set, true_tagged_set):
         count = 0.0
         count_of_true = 0.0
@@ -170,6 +174,7 @@ class Searcher:
                         self.confusion_matrix[true_tagged_word.split("_")[1]][tagged_set[index_of_sentence][index_of_word].split("_")[1]] += 1
         return count_of_true
 
+        # extracting relevant tags for each word
     def extract_relevant_tags(self, index, sentence):
         if index < 0:
             return (self.tags["start"],)
@@ -188,15 +193,15 @@ class Searcher:
                 return (self.tags[self.open_tags_digit],)
             return(self.get_open_tags())
 
+        # open parts of speech in english
     def get_open_tags(self):
-        #open parts of speech in english
         open_tags = ("NNP",)
         to_return = []
         for item in open_tags:
             to_return.append(self.tags[item])
         return tuple(to_return)
 
-
+        # subroutine for probabilities calculation
     def create_exponential_permutations_matrix(self,relevant_tags,previous_tag,last_tag, word):
         permutation_matrix = lil_matrix((len(relevant_tags), self.gradient_ascent.feature_maker.number_of_dimensions))
         current_index = 0
@@ -205,6 +210,7 @@ class Searcher:
             current_index += 1
         return csr_matrix(permutation_matrix)
 
+        # some rule-based inference (described in the report, for both models)
     def postprocessing(self, supposed_tag, word):
         actual_tag = supposed_tag
         if supposed_tag == self.tags["start"]:
@@ -215,7 +221,7 @@ class Searcher:
                     actual_tag = self.tags[self.feature_maker.k_most_seen_tags[word][0]]
             if self.feature_maker.k_most_seen_tags[word][0] == self.tags["NNP"] and not word[0].isupper():
                 actual_tag = self.tags["NN"]
-        else:
+        elif(self.feature_maker.extended):
             for suffix in self.NN_specific_suffixes:
                 if word[-len(suffix):] == suffix:
                     actual_tag = self.tags["NN"]
--- main.py
+++ main.py
@@ -7,20 +7,24 @@ import GradientAscent as g
 import random
 
 if __name__ == '__main__':
+    extended_model = False
+
+
+
     start = d.datetime.now()
     tags, tags_count, inverse_tag_count = Preprocessing.preprocessing()
     values = sorted(tags_count.values())
     print(tags_count)
     values.reverse()
     Preprocessing.percentage_of_unknown_words()
-    little_test, pure_little_test = Preprocessing.create_little_test(0.01)
+    little_test, pure_little_test = Preprocessing.create_little_test(1)
     unigrams, w = Preprocessing.get_ngrams(1)
     bigrams, w = Preprocessing.get_ngrams(2)
     trigrams, words = Preprocessing.get_ngrams(3)
-    fmaker = featureMaker.feature_maker("<<>>",5,True,1)
+    fmaker = featureMaker.feature_maker("<<>>", 5, extended_model, 1)
     fmaker.init_all_params(unigrams,bigrams,trigrams)
     hist = Preprocessing.histogram_of_ngrams(3)
-    lambda_values =[1000]
+    lambda_values =[10,]
     for value in lambda_values:
         gascent = g.gradient_ascent(fmaker.number_of_dimensions,value,fmaker,hist)
         hope = gascent.gradient_ascent()
